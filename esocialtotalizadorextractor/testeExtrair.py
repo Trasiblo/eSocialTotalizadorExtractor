7d763a42cdcd2afa8cdada63e5d9a8bdbd8f3492
Trasiblo Martins <trasiblo@hotmail.com>
2024-03-07 21:07:20 -0300

    remover import sem uso

--- esocialtotalizadorextractor/testeExtrair.py
+++ esocialtotalizadorextractor/testeExtrair.py
@@ -1,5 +1,4 @@
 from tqdm import tqdm
-import csv
 from utilitarios import *
 import extracao
 import esocialtotalizadorextractor.banco.s5001
@@ -9,7 +8,7 @@ import esocialtotalizadorextractor.banco.s3000
 
 #informar nome da pasta com os xmls de retorno da Consulta Processamento de Lote
 diretorio = '/home/tasso/Projects/esocial/pyTotalizadores/' + '3678466'
-diretorio = "I:\\USUARIO\\Área de Trabalho\\2024\\2024-ESOCIAL\\2024-ERROS_TEMPOS_ASPEC\\3678466"
+#diretorio = "I:\\USUARIO\\Área de Trabalho\\2024\\2024-ESOCIAL\\2024-ERROS_TEMPOS_ASPEC\\3678466"
 #diretorio = "I:\\USUARIO\\Área de Trabalho\\2024\\2024-ESOCIAL\\2024-ERROS_TEMPOS_ASPEC\\2024"
 
 #dados S-5001
