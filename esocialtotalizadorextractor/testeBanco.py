f89d09bf7186bd50c7d4c698eab7699f21ebb58e
Trasiblo Martins <trasiblo@hotmail.com>
2024-03-07 21:08:48 -0300

    melhoramento para usar pandas e exportar dados para csv

--- esocialtotalizadorextractor/testeBanco.py
+++ esocialtotalizadorextractor/testeBanco.py
@@ -1,5 +1,7 @@
 import esocialtotalizadorextractor.banco.db as db
+import esocialtotalizadorextractor.banco as conexao
 import locale
+import pandas as pd
 
 def formatDinheiro(vlr):
     #locale.setlocale(locale.LC_ALL, 'pt')
@@ -7,16 +9,27 @@ def formatDinheiro(vlr):
     valor = locale.currency(vlr, grouping=False, symbol=None)
     return valor
 
-comunicacao = db.conn
+
 #db.criar_tabela_todo()
 #db.add_tarefa('teste')
 
 #db.criar_tabela_s3000(comunicacao)
 #db.criar_tabela_s5001(comunicacao)
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
-valores = db.get_valor(comunicacao)
 
+valores = db.get_valor(conexao.conn)
+valores.row_factory = dict_factory
 valor = valores.fetchall()
+
+df1 = pd.DataFrame(valor)
+
+print(df1)
+df1.to_csv('teste.csv',sep=';', index=False)
 
 colunas = valores.description
 
@@ -26,14 +39,15 @@ colunas = valores.description
 
 #converter dados
 
-for r in valor:
-    rg = []
-    for c in r:
-        
-        if (type(c) == float):
-            c = formatDinheiro(c)
-        rg.append(c)
-    print(rg)
-        
+#for r in valor:
+#    rg = []
+#   for c in r:
+#       
+#       if (type(c) == float):
+#           c = formatDinheiro(c)
+#       rg.append(c)
+#   print(rg)
+
+
 
 #print(valor[0][0])
