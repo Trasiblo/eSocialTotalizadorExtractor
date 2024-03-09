361ae6228838e2466b96de0fda7d3398e2da83ab
Trasiblo Martins <trasiblo@hotmail.com>
2024-03-09 19:23:20 -0300

    retorno de dados analiticos do totalizador

--- esocialtotalizadorextractor/banco/db.py
+++ esocialtotalizadorextractor/banco/db.py
@@ -29,3 +29,27 @@ def get_valor(conn):
     group by s.perApur
     """
     return conn.execute(sql)
+
+def get_valor_analitico(conn):
+    sql = """
+    with ex as (
+    select * from s3000 ex
+    where
+    ex.infoExclusao_tpEvento = 'S-1200'),
+
+    validos as( 
+    select t.* from s5001 t
+    left join ex on t.nrRecArqBase = ex.infoExclusao_nrRecEvt and ex.infoExclusao_perApur = t.perApur and ex.infoExclusao_cpfTrab = t.cpfTrab
+    where ex.id is null)
+
+
+    --select s.matricula, s.valor_baseInss from validos s
+    --select s.matricula, SUM(s.valor_baseInss) from validos s
+    --where s.perApur = '2024-01'
+    --and matricula is not null
+
+    select 
+    s.*
+    from validos s 
+    """
+    return conn.execute(sql)
