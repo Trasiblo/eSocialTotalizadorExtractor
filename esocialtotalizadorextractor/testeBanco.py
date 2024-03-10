a508e9d0758019e15237e6672bb41d6bbbc71398
Trasiblo Martins <trasiblo@hotmail.com>
2024-03-10 14:55:19 -0300

    ajuste nos teste para geração limpa de um arquivo excel

--- esocialtotalizadorextractor/testeBanco.py
+++ esocialtotalizadorextractor/testeBanco.py
@@ -1,56 +1,18 @@
-import banco.db as db
 import banco as conexao
-import locale
 import pandas as pd
 
 
-def formatDinheiro(vlr):
-    #locale.setlocale(locale.LC_ALL, 'pt')
-    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
-    valor = locale.currency(vlr, grouping=False, symbol=None)
-    return valor
+rel = conexao.relatorio
 
-
-#db.criar_tabela_todo()
-#db.add_tarefa('teste')
-
-#db.criar_tabela_s3000(comunicacao)
-#db.criar_tabela_s5001(comunicacao)
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
-#valores = db.get_valor(conexao.conn)
-valores = db.get_valor_analitico(conexao.conn)
-valores.row_factory = dict_factory
-valor = valores.fetchall()
-
+valor = rel.get_valor()
 df1 = pd.DataFrame(valor)
 
-print(df1)
-df1.to_csv('teste.csv',sep=';', index=False)
-df1.to_excel('teste.xlsx', index=False, float_format='%.2f')
+valor = rel.get_valor_analitico()
+df2 = pd.DataFrame(valor)
 
-colunas = valores.description
+arqExcel = pd.ExcelWriter('teste.xlsx')
 
-#pegar nome das colunas
-#for r1 in colunas:
-    #print(r1[0])
+df1.to_excel(arqExcel, index=False, float_format='%.2f', sheet_name='Resumo')
+df2.to_excel(arqExcel, index=False, float_format='%.2f', sheet_name='Analitica')
 
-#converter dados
-
-#for r in valor:
-#    rg = []
-#   for c in r:
-#       
-#       if (type(c) == float):
-#           c = formatDinheiro(c)
-#       rg.append(c)
-#   print(rg)
-
-
-
-#print(valor[0][0])
+arqExcel.close()
